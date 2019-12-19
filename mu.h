f1abcd39fcf8aafef85d16dcee268b15f8bf3541
Chinmaya Dehury <chinmaya.dehury007@gmail.com>
2019-12-19 13:32:39 -0500

    2

--- mu.h
+++ mu.h
@@ -12,7 +12,12 @@ getMu(graph G)
         for(int j=0;j<G.nodes;j++)
             sum+=G.adjMat[i][j];
         for(int j=0;j<G.nodes;j++)
-            myfile<<(float)((G.adjMat[i][j]*1.0)/(sum*1.0))<<",";
+        {
+            if(j==G.nodes-1)
+                myfile<<(float)((G.adjMat[i][j]*1.0)/(sum*1.0));
+            else
+                myfile<<(float)((G.adjMat[i][j]*1.0)/(sum*1.0))<<",";
+        }
         myfile<<endl;
     }
 }
