f57e3726136de9b53f6f3a21596fb0001be37dcb
Chinmaya Dehury <chinmaya.dehury007@gmail.com>
2019-12-18 23:24:57 -0500

    1

---
+++ mu.h
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include "graph.h"
+using namespace std;
+getMu(graph G)
+{
+    ofstream myfile;
+    myfile.open("output.txt");
+    int sum=0;
+    for(int i=0;i<G.nodes;i++)
+    {
+        sum=0;
+        for(int j=0;j<G.nodes;j++)
+            sum+=G.adjMat[i][j];
+        for(int j=0;j<G.nodes;j++)
+            myfile<<(float)((G.adjMat[i][j]*1.0)/(sum*1.0))<<",";
+        myfile<<endl;
+    }
+}
